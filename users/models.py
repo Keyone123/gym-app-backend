5e09bcc5179ec31baca95591eecfda6b532e9293
Kaua Marques <mkaua1011@gmail.com>
2026-01-17 15:58:55 -0300

    Fix: refatorando ainda mais o modal

--- users/models.py
+++ users/models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class User(AbstractUser):
-    isAdmin = models.BooleanField(default=False)
-
     def __str__(self):
         return self.username
--- users/serializers.py
+++ users/serializers.py
@@ -6,36 +6,14 @@ User = get_user_model()
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ("id", "username", "email", "password", "isAdmin")
-
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Email já está em uso")
-        return value
-
-    def validate_username(self, value):
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError("Username já está em uso")
-        return value
+        fields = ("id", "username", "email", "password")
 
     def create(self, validated_data):
-        try:
-            user = User.objects.create_user(
-                username=validated_data["username"],
-                email=validated_data["email"],
-                password=validated_data["password"],
-                isAdmin=validated_data.get("isAdmin", False),
-            )
-        except IntegrityError:
-            raise serializers.ValidationError(
-                {"detail": "Erro ao criar usuário"}
-            )
-
-        return user
+        return User.objects.create_user(**validated_data)
 
 
 class UserSerializer(serializers.ModelSerializer):
